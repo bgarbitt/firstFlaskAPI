3824705abf4c29f82331ddbfd8a051243e1cd686
Brett <bgarbitt@ualberta.ca>
2017-11-09 18:11:58 +0000

    Malformed Bearer Token Test

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, request, make_response, jsonify
 from flask.views import MethodView
 
 from project.server import bcrypt, db
-from project.server.models import User
+from project.server.models import User, BlacklistToken
 
 auth_blueprint = Blueprint('auth', __name__)
 
@@ -62,14 +62,23 @@ class LoginAPI(MethodView):
             user = User.query.filter_by(
                 email=post_data.get('email')
               ).first()
-            auth_token = user.encode_auth_token(user.id)
-            if auth_token:
-                responseObject = {
-                    'status': 'success',
-                    'message': 'Successfully logged in.',
+            if user and bcrypt.check_password_hash(
+                    user.password, post_data.get('password')
+            ):
+                auth_token = user.encode_auth_token(user.id)
+                if auth_token:
+                    responseObject = {
+                        'status': 'success',
+                        'message': 'Successfully logged in.',
                     'auth_token': auth_token.decode()
+                    }
+                    return make_response(jsonify(responseObject)), 200
+            else:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'User does not exist.'
                 }
-                return make_response(jsonify(responseObject)), 200
+                return make_response(jsonify(responseObject)), 404
         except Exception as e:
             print(e)
             resonseObject = {
@@ -87,7 +96,14 @@ class UserAPI(MethodView):
         # get the auth token
         auth_header = request.headers.get('Authorization')
         if auth_header:
-            auth_token = auth_header.split(" ")[1]
+            try:
+                auth_token = auth_header.split(" ")[1]
+            except IndexError:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Bearer token malformed.'
+                }
+                return make_response(jsonify(responseObject)), 401
         else:
             auth_token = ''
         if auth_token:
@@ -116,13 +132,56 @@ class UserAPI(MethodView):
             }
             return make_response(jsonify(responseObject)), 401
                         
-          
+class LogoutAPI(MethodView):
+    """
+    Logout Resource
+    """
 
-        
+    def post(self):
+        # get auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            response = User.decode_auth_token(auth_token)
+            if not isinstance(response, str):
+                # mark the token as blacklisted
+                blacklist_token = BlacklistToken(token=auth_token)
+                try:
+                    # insert the token
+                    db.session.add(blacklist_token)
+                    db.session.commit()
+                    responseObject = {
+                        'status': 'success',
+                        'message': 'Successfully logged out.'
+                    }
+                    return make_response(jsonify(responseObject)), 200
+                except Exception as e:
+                    responseObject = {
+                        'status': 'fail',
+                        'message': e
+                    }
+                    return make_response(jsonify(responseObject)), 200
+            else:
+                responseObject = {
+                    'status': 'fail',
+                    'message': response
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 403
+            
 # define the API resources
 registration_view = RegisterAPI.as_view('register_api')
 login_view = LoginAPI.as_view('login_api')
 user_view = UserAPI.as_view('user_api')
+logout_view = LogoutAPI.as_view('logout_api')
 
 # add Rules for API Endpoints
 auth_blueprint.add_url_rule(
@@ -141,4 +200,10 @@ auth_blueprint.add_url_rule(
     '/auth/status',
     view_func=user_view,
     methods=['GET']
+)
+
+auth_blueprint.add_url_rule(
+    '/auth/logout',
+    view_func=logout_view,
+    methods=['POST']
 )
--- project/tests/test_auth.py
+++ project/tests/test_auth.py
@@ -5,7 +5,7 @@ import json
 import time
 
 from project.server import db
-from project.server.models import User
+from project.server.models import User, BlacklistToken
 from project.tests.base import BaseTestCase
 
 def register_user(self, email, password):
@@ -80,6 +80,16 @@ class TestAuthBlueprint(BaseTestCase):
             self.assertTrue(response.content_type == 'application/json')
             self.assertEqual(response.status_code, 200)
 
+    def test_non_registeered_user_login(self):
+        """ Test for login of non-registered user """
+        with self.client:
+            response = login_user(self, 'test@gmail.com', 'test123')
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(data['message'] == 'User does not exist.')
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 404)
+            
     def test_user_status(self):
         """ Test for user status """
         with self.client:
@@ -115,10 +125,10 @@ class TestAuthBlueprint(BaseTestCase):
             resp_login = login_user(self, 'test@gmail.com', 'test123')
             data_login = json.loads(resp_login.data.decode())
             self.assertTrue(data_login['status'] == 'success')
-            self.assertTrue(data_login['message'] == 'Succesfully logged in.')
+            self.assertTrue(data_login['message'] == 'Successfully logged in.')
             self.assertTrue(data_login['auth_token'])
             self.assertTrue(resp_login.content_type == 'application/json')
-            self.assertEqual(resp_login.status_cude, 200)
+            self.assertEqual(resp_login.status_code, 200)
             # valid token logout
             response = self.client.post(
                 '/auth/logout',
@@ -168,6 +178,85 @@ class TestAuthBlueprint(BaseTestCase):
             self.assertTrue(
                 data['message'] == 'Signature expired. Please log in again.')
             self.assertEqual(response.status_code, 401)
-        
+
+    def test_valid_blacklisted_token_logout(self):
+        """ Test for logout after a valid token gets blacklisted """
+        with self.client:
+            # user registration
+            resp_register = register_user(self, 'test@gmail.com', 'test123')
+            data_register = json.loads(resp_register.data.decode())
+            self.assertTrue(data_register['status'] == 'success')
+            self.assertTrue(
+                data_register['message'] == 'Successfully registered.')
+            self.assertTrue(data_register['auth_token'])
+            self.assertTrue(resp_register.content_type == 'application/json')
+            self.assertEqual(resp_register.status_code, 201)
+            # user login
+            resp_login = login_user(self, 'test@gmail.com', 'test123')
+            data_login=json.loads(resp_login.data.decode())
+            self.assertTrue(data_login['status'] == 'success')
+            self.assertTrue(data_login['message'] == 'Successfully logged in.')
+            self.assertTrue(data_login['auth_token'])
+            self.assertTrue(resp_login.content_type == 'application/json')
+            self.assertEqual(resp_login.status_code, 200)
+            # blacklist a valid token
+            blacklist_token = BlacklistToken(
+                token=json.loads(resp_login.data.decode())['auth_token'])
+            db.session.add(blacklist_token)
+            db.session.commit()
+            # blacklisted valid token logout
+            response = self.client.post(
+                '/auth/logout',
+                headers=dict(
+                    Authorization='Bearer ' + json.loads(
+                        resp_login.data.decode()
+                    )['auth_token']
+                )
+            )
+            data=json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(data['message'] == 'Token blacklisted. Please log in again.')
+            self.assertEqual(response.status_code, 401)
+
+    def test_valid_blacklisted_token_user(self):
+        """ Test for user status with a blacklisted valid token """
+        with self.client:
+            resp_register = register_user(self, 'test123@gmail.com', 'test123')
+            # blacklist a valid token
+            blacklist_token = BlacklistToken(
+                token=json.loads(resp_register.data.decode())['auth_token'])
+            db.session.add(blacklist_token)
+            db.session.commit()
+            response = self.client.get(
+                '/auth/status',
+                headers=dict(
+                    Authorization='Bearer ' + json.loads(
+                        resp_register.data.decode()
+                    )['auth_token']
+                )
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(data['message'] == 'Token blacklisted. Please log in again.')
+            self.assertEqual(response.status_code, 401)
+                            
+    def test_user_status_malformed_bearer_token(self):
+        """ Test for user status with malformed bearer token """
+        with self.client:
+            resp_register = register_user(self, 'test@gmail.com', 'test123')
+            response = self.client.get(
+                '/auth/status',
+                headers=dict(
+                    Authorization='Bearer' + json.loads(  # Bearer doesn't have a space
+                        resp_register.data.decode()
+                    )['auth_token']
+                )
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(data['message'] == 'Bearer token malformed.')
+            self.assertEqual(response.status_code, 401)
+
+            
 if __name__ == '__main__':
     unittest.main()
